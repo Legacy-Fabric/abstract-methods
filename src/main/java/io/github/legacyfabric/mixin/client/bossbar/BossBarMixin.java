302e39522a86d1376e60922e5b5c8eab387c5e74
Luke Schwager <luke@blucobalt.dev>
2020-11-23 22:56:53 -0800

    it works™

---
+++ src/main/java/io/github/legacyfabric/mixin/client/bossbar/BossBarMixin.java
@@ -0,0 +1,15 @@
+package io.github.legacyfabric.mixin.client.bossbar;
+
+import net.minecraft.entity.boss.BossBar;
+import net.minecraft.entity.boss.BossBarProvider;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+@Mixin(BossBar.class)
+public class BossBarMixin {
+    /**
+     * @author blucobalt
+     */
+    @Overwrite
+    public static void method_3301(BossBarProvider provider, boolean bl) {}
+}
---
+++ src/main/java/io/github/legacyfabric/mixin/client/bossbar/EnderDragonEntityRendererMixin.java
@@ -0,0 +1,21 @@
+package io.github.legacyfabric.mixin.client.bossbar;
+
+import net.minecraft.client.render.entity.EnderDragonEntityRenderer;
+import net.minecraft.entity.boss.dragon.EnderDragonEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import static net.minecraft.entity.boss.BossBar.*;
+
+@Mixin(EnderDragonEntityRenderer.class)
+public class EnderDragonEntityRendererMixin {
+    @Inject(method = "render", at = @At("TAIL"))
+    private void render(EnderDragonEntity dragon, double d, double e, double f, float g, float h, CallbackInfo ci) {
+        field_4188 = dragon.getHealth() / dragon.getMaxHealth();
+        field_4189 = 100;
+        field_4190 = dragon.getName().asFormattedString();
+        field_4191 = true;
+    }
+}
